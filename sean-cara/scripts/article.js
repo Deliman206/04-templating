af8caa2ab3e3241cf5f994594f94ba8b9c12b4a5
Sean Miller <Sean@Seans-MacBook-Pro.local>
2018-03-16 10:27:31 -0700

    filled template & rendered articles

---
+++ sean-cara/scripts/article.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let articles = [];
+
+function Article (rawDataObj) {
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
+}
+
+Article.prototype.toHtml = function() {
+  // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+
+  // 1. Get the template from the HTML document
+  let template = $('#blog-template').html();
+  // 2. Use Handlebars to "compile" the HTML
+  let templateRender = Handlebars.compile(template);
+  // 3. Do not forget to return the HTML from this method
+  
+  // REVIEW: If your template will use properties that aren't on the object yet, add them.
+  // Since your template can't hold any JS logic, we need to execute the logic here.
+  // The result is added to the object as a new property, which can then be referenced by key in the template.
+  // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  
+  
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  
+  // REVIEW: The ternary operator above accomplishes this same logic.
+  // if(this.publishedOn) {
+    //   this.publishStatus = `published ${this.daysAgo} days ago`;
+    // } else {
+      //   this.publishStatus = '(draft)';
+      // }
+      
+      // DONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  
+  console.log(templateRender);
+  return templateRender(this);
+};
+
+// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
+//The .sort() method can pass 2 params so it needs to contain them in parens. The .forEach() method can only pass 1 param so it does not NEED parens, however having parens around a single param is kosher as well.
+rawData.sort((a,b) => {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(articleObject => {
+  articles.push(new Article(articleObject));
+});
+
+articles.forEach(article => {
+  $('#articles').append(article.toHtml());
+});
